7345912c4d58d71eb5ef616442ef100f5236a139
Oksana Omelchenko <kuzheloksana@gmail.com>
2021-02-14 21:01:23 +0200

    services mobile

--- script/script.js
+++ script/script.js
@@ -1,117 +1,120 @@
 //   Burger menu screen <= 996
 
-const burger = document.querySelector('#burger');
-const burgerMenu = document.querySelector('.js-navbar');
+const burger = document.querySelector("#burger");
+const burgerMenu = document.querySelector(".js-navbar");
 
 burger.onclick = function () {
-    burger.classList.toggle('is-active');
-    burgerMenu.classList.toggle('is-open');
-    if (contactBtn.classList.contains('is-active')) {
-        contactBtn.classList.remove('is-active');
-        contactBlock.classList.remove('is-open');
-    }
+  burger.classList.toggle("is-active");
+  burgerMenu.classList.toggle("is-open");
+  if (contactBtn.classList.contains("is-active")) {
+    contactBtn.classList.remove("is-active");
+    contactBlock.classList.remove("is-open");
+  }
 };
 
 //   Contact btn screen <= 996
 
-const contactBtn = document.querySelector('#contact-btn');
-const contactBlock = document.querySelector('.js-contact-block');
+const contactBtn = document.querySelector("#contact-btn");
+const contactBlock = document.querySelector(".js-contact-block");
 
 contactBtn.onclick = function () {
-    contactBtn.classList.toggle('is-active');
-    contactBlock.classList.toggle('is-open');
-    if (burger.classList.contains('is-active')) {
-        burger.classList.remove('is-active');
-        burgerMenu.classList.remove('is-open');
-    }
+  contactBtn.classList.toggle("is-active");
+  contactBlock.classList.toggle("is-open");
+  if (burger.classList.contains("is-active")) {
+    burger.classList.remove("is-active");
+    burgerMenu.classList.remove("is-open");
+  }
 };
 
 // Open-close submenu screen <=996
 
-const menuItems = document.querySelectorAll('.js-menuItems');
+const menuItems = document.querySelectorAll(".js-menuItems");
 
-menuItems.forEach(item => {
-    item.addEventListener('click', (e) => {
-        if (e.target.classList.contains("navbar_center_menu-link")) {
-            e.preventDefault();
-            item.classList.toggle('is-open');
-        }
-        else {
-            return;
-        }
+menuItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (e.target.classList.contains("navbar_center_menu-link")) {
+      e.preventDefault();
+      item.classList.toggle("is-open");
+    } else {
+      return;
     }
-    );
-}
-);
-
-
+  });
+});
 
 // Slider SWIPER
 
-const swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    effect: 'fade',
-    autoplay: {
-        delay: 10000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+const swiper = new Swiper(".swiper-container", {
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-
 
 // -------------LIGHTBOX----------
 
-function lightbox(itemSelector, modalSelector, modalImageSelector) {
-    const items = document.querySelectorAll(itemSelector);
-    const modal = document.querySelector(modalSelector);
-    const modalImage = document.querySelector(modalImageSelector);
+function lightbox(
+  openSelector,
+  imgSelector,
+  modalSelector,
+  modalImageSelector,
+  closeSelector
+) {
+  const openBnts = document.querySelectorAll(openSelector);
+  const smallImgs = document.querySelectorAll(imgSelector);
+  const modal = document.querySelector(modalSelector);
+  const modalImage = document.querySelector(modalImageSelector);
+  const closeBtn = document.querySelector(closeSelector);
 
-    // Open Modal
+  // Open Modal
 
-    items.forEach((item) => {
-      item.addEventListener("click", handleOpenModal);
+  openBnts.forEach((btn, i) => {
+    btn.addEventListener("click", handleOpenModal);
 
-      function handleOpenModal(event) {
-        event.preventDefault();    
-       let img=event.currentTarget.firstElementChild;
-        if (event.currentTarget === event.target) {
-          return;
-        } else {
-          modal.classList.add("is-open");
-          modalImage.alt = img.alt;
-          modalImage.src = img.dataset.source;
-
-          window.addEventListener("keydown", handleCloseModalEsc);
-          modal.addEventListener("click", handleCloseModalBackground);
-        }
-      }
-    });
-
-    // Close Modal
-
-    function handleCloseModal() {
-      modalImage.alt = "";
-      modalImage.src = "";
-      modal.classList.remove("is-open");
+    function handleOpenModal(event) {
+      event.preventDefault();
+      let img = smallImgs[i];
+      modal.classList.add("is-open");
+      modalImage.alt = img.alt;
+      modalImage.src = img.dataset.source;
+      closeBtn.addEventListener("click", handleCloseModal);
+      window.addEventListener("keydown", handleCloseModalEsc);
+      modal.addEventListener("click", handleCloseModalBackground);
     }
+  });
 
-    function handleCloseModalEsc(event) {
-      if (event.code === "Escape") {
-        handleCloseModal();
-      }
-    }
+  // Close Modal
 
-    function handleCloseModalBackground(event) {
-      if (event.target === modalImage) {
-        return;
-      }
+  function handleCloseModal() {
+    modalImage.alt = "";
+    modalImage.src = "";
+    modal.classList.remove("is-open");
+  }
+
+  function handleCloseModalEsc(event) {
+    if (event.code === "Escape") {
       handleCloseModal();
-      modal.removeEventListener("click", handleCloseModalBackground);
     }
   }
 
+  function handleCloseModalBackground(event) {
+    if (event.target === modalImage) {
+      return;
+    }
+    handleCloseModal();
+    modal.removeEventListener("click", handleCloseModalBackground);
+  }
+}
 
-  lightbox(".js-lightBoxItem", ".js-lightbox", ".lightbox__image");
+lightbox(
+  ".js-lightboxOpen",
+  ".js-smallImg",
+  ".js-lightbox",
+  ".lightbox__image",
+  ".js-lightboxClose"
+);
