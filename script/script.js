d36d865fe08ebe1e87c53e6a40c030ce66bd5499
Oksana Omelchenko <kuzheloksana@gmail.com>
2021-02-15 10:52:57 +0200

    add footer

--- script/script.js
+++ script/script.js
@@ -47,7 +47,7 @@ const swiper = new Swiper(".swiper-container", {
   spaceBetween: 30,
   effect: "fade",
   autoplay: {
-    delay: 5000,
+    delay:5000,
     disableOnInteraction: false,
   },
   pagination: {
